72f1ceeffeacd425e03c48efc19118ef75522682
Mayenga <bndicunguye@gmail.com>
2019-08-12 10:16:43 -0400

    adding initial setup burger

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ("double cheese burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("quarter pound burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("bacon beef burger");
